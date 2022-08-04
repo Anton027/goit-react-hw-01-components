ab68bec3b976d772ba59ca1ff9dacaadab047e04
Anton Kutsim <kutsimanton@gmail.com>
2022-08-04 16:08:12 +0300

    p2
    case01.begin

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,40 @@
+import React, { Fragment } from "react";
+const InfoProfile = ({ label, quantity }) => { 
+  return (
+    <li>
+      <span>{label}</span>
+      <span>{quantity}</span>
+    </li>
+  )
+};
+// const InfoUser = ({ label }) => {
+//   return <p>{label}</p>
+// };
+
+const UserInfo = ({ label, text, info, imgUrl}) => {
+  return (
+    <Fragment>
+      <img src={imgUrl} alt={label} width='120'/>
+      <p>{label}</p>
+      <p>{text}</p>
+      <p>{info}</p>
+    </Fragment>
+  )
+};
+
+export const Profile = ({ user:
+  { username, tag, location, avatar, stats }
+}) => {
+  return (
+    <div >
+      <div>
+        <UserInfo imgUrl={avatar} label={username} text={tag} info={location} />
+      </div>
+      <ul>
+        <InfoProfile label={`Followers`} quantity={stats.followers} />
+        <InfoProfile label={`Views`} quantity={stats.views} />
+        <InfoProfile label={`Likes`} quantity={stats.likes}/>
+      </ul>
+    </div>
+  );
+}
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,6 @@
+import data from 'components/Statistics/data.json';
+
+
+
+{/* <Statistics title="Upload stats" stats={data} />
+<Statistics stats={data} /> */}
